65a899c385fc32eabd3f20b6944500837098fd19
Scott LaVigne <pyrated@gmail.com>
2016-09-01 02:13:39 -0700

    Fix RTD build

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from setuptools import setup, find_packages
-import os
 import vinyl
 
-
-_REQUIRES = []
-
-# We cannot install llvmlite on READTHEDOCS
-if os.environ.get('READTHEDOCS') != 'True':
-    _REQUIRES.append('llvmlite')
 
 setup(name='vinylang',
       version=vinyl.__version__,
@@ -19,7 +12,7 @@ setup(name='vinylang',
       long_description=open('README.rst').read(),
       download_url='https://github.com/pyrated/vinyl.git',
       packages=find_packages(),
-      requires=_REQUIRES,
+      requires=open('requirements.txt').read().splitlines(),
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Programming Language :: Python :: 3.5'
